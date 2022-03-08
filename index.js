0144c1b7ddabb82c2c465e2f17f55b4304e70a25
John Devaney <l00161731@student.lyit.ie>
2022-03-08 11:38:12 +0000

    I added mikey ban

--- index.js
+++ index.js
@@ -8,15 +8,53 @@ config({
 
 bot.on("ready", () => {
     console.log("The bot is online.");
-    
-    bot.user.setActivity("you playing with my tits", 
-        {
+
+    // bot.user.setPresence({
+    //     status: "Booba",
+    //     game: {
+    //         name: "Boing",
+    //         type: "WATCHING"
+    //     }
+    // })
+
+    // console.log (`I'm online!, my name is ${client.user.username}`);
+     
+    bot.user.setPresence({
+        status: "Online",
+        game: {
+            name: "you being quiet",
             type: "WATCHING"
+        }
     });
 })
-
+var msg = "cool";
+var leigh = "HENeedsMOREMilk";
+var john = "JohnDev";
 bot.on("messageCreate", (message) => {
-    console.log(`This is what message I read: ${message}`);
+    console.log(`Some one stupid said: ${message}`);
+    
+    //var gameName = message.author.activity.name;
+    var guild = bot.get_guild(int(entry['guild_id']));
+    var member = guild.get_member(int(entry['user_id']));
+
+
+    
+    if (member.activity.name.lower() == 'league of legends')
+    {
+        member.send('League Ban!');
+        member.ban(reason='Being a nerd');
+        message.channel.send($member + " has been banned for playing "+ member.activity.name);
+    }
+
+    // if(message.author.id === leigh){
+    //     message.channel.send("Shut the fuck up Leigh!");
+    // }
+    // // if message includes a certain word
+    // if (message.content.includes(msg)){
+    //     message.channel.send(msg);
+    //     msg="";
+        
+    // }
 })
 
 bot.login(process.env.TOKEN)
